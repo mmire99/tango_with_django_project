714ef6d28883379a965d444c1963f35507ff30ca
Mohamed Abdi Mire <2468192a@student.gla.ac.uk>
2021-02-12 16:04:33 +0000

    10th chapter

--- rango/views.py
+++ rango/views.py
@@ -9,8 +9,7 @@ from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-
-
+from datetime import datetime
 
 def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
@@ -19,12 +18,20 @@ def index(request):
 	context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
 	context_dict['categories'] = category_list
 	context_dict['pages'] = page_list
-	return render(request, 'rango/index.html', context=context_dict)
+	visitor_cookie_handler(request)
+	response = render(request, 'rango/index.html', context=context_dict)
+	return response
+
 
 def about(request):
 	print(request.method)
 	print(request.user)
-	return render(request, 'rango/about.html', {})
+	context_dict = {}
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
+	response = render(request, 'rango/about.html', context=context_dict)
+	
+	return response
 
 def show_category(request, category_name_slug):
 	context_dict = {}
@@ -135,6 +142,30 @@ def user_login(request):
 			return HttpResponse("Invalid login details supplied.")
 	else:
 		return render(request, 'rango/login.html')
+
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val
+
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request,
+	'last_visit',
+	str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+	'%Y-%m-%d %H:%M:%S')
+	# If it's been more than a day since the last visit...
+	if (datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+		# Update the last visit cookie now that we have updated the count
+		request.session['last_visit'] = str(datetime.now())
+	else:
+	# Set the last visit cookie
+		request.session['last_visit'] = last_visit_cookie
+		# Update/set the visits cookie
+	request.session['visits'] = visits
 
 @login_required
 def restricted(request):
